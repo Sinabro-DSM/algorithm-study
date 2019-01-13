4d9252fa531989e8dc13c76bd3cd4848c6249334
khg0712 <k7120792@gmail.com>
2019-01-13 15:34:13 +0900

    변장 문제 코드

---
+++ 김형규/2019_01_08_code.js
@@ -0,0 +1,15 @@
+function solution(clothes) {
+    var answer = 1;
+    const store = {};
+    for(let i = 0; i < clothes.length; i++) {
+        if(store[clothes[i][1]] == undefined)
+            store[clothes[i][1]] = 1;
+        else
+            store[clothes[i][1]] += 1;
+    }
+    
+    for(let i in store) {
+        answer *= store[i]+1
+    }
+    return answer - 1;
+}
