4b71aa0fa732b85fbf6f1de4ad8aefd05700a6ad
No-YE <nye7181@gmail.com>
2019-04-04 10:11:50 +0900

    2019-03-26 노영은 회고 & 코드

---
+++ 노영은/2019-03-26_code.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class Solution {
+  public int solution(int[] people, int limit) {
+    int answer=0;
+    int i=0, j=people.length-1;
+    
+    Arrays.sort(people);
+        
+    while(i<=j) {
+      if(people[j]+people[i]<=limit) {
+          j--;
+          i++;
+      } else {
+          j--;
+      }
+      answer++;
+    }
+    
+    return answer;
+  }
+}
